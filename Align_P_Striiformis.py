45e22f97a39d06b2f2e337f102fded5647aaa032
jessicakan789 <83308735+jessicakan789@users.noreply.github.com>
2022-01-19 16:04:00 +0000

    Create Align_P_Striiformis.py

---
+++ Align_P_Striiformis.py
@@ -0,0 +1,33 @@
+# Count contigs and alignments
+
+from collections import Counter
+
+Contig_list = list(range(1, 283, 1))
+Align_list = []
+
+# Read line by line
+    
+with open('blastn_Puccinia_Striiformis.txt') as file:
+
+    for line in file:
+        
+        if line.startswith('Sequences producing significant alignments'):
+            Align_list += "1"
+            
+        elif line.startswith('***** No hits found *****'):
+            Align_list += "0"
+
+print(Contig_list)
+print(Align_list)
+
+# Tabulate using pandas
+        
+import pandas as pd
+
+tableData = [Contig_list, Align_list]
+Align = pd.DataFrame(tableData)
+print(Align)
+
+# Write out to csv
+
+Align.to_csv(r'Align_P_Striiformis.csv')
