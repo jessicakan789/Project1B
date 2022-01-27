51829c9d95219fdaf170cf60de3fc38d78c8e8e7
jessicakan789 <83308735+jessicakan789@users.noreply.github.com>
2022-01-27 20:55:09 +0000

    Update Amino_acid_count.py

--- Amino_acid_count.py
+++ Amino_acid_count.py
@@ -4,7 +4,7 @@ amino_acids = []
 # Open amino acid file
 with open("Septo_prot.aa.fasta") as file:
     for line in file: # read line by line
-        if not line.startswith("<"): # exclude headers
+        if not line.startswith(">"): # exclude headers
             amino_acids += line # append list
             
 # Create another empty list
